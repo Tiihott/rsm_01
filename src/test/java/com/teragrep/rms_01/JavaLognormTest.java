814c842de7ddb91a8319d0053e427ec2d20c86e0
Tiihott <48@teragrep.com>
2024-11-27 09:38:28 +0200

    Implementation of all required components complete. Builds correctly and Liblognorm version check test passes assertion.

--- src/test/java/com/teragrep/rms_01/JavaLognormTest.java
+++ src/test/java/com/teragrep/rms_01/JavaLognormTest.java
@@ -43,64 +43,20 @@
  * Teragrep, the applicable Commercial License may apply to this file if you as
  * a licensee so wish it.
  */
-#include <stddef.h>
-#include <string.h>
-#include <liblognorm/liblognorm.h>
-#include <liblognorm/lognorm.h>
+package com.teragrep.rms_01;
 
-/* compiler commands for this file, may need additional compiler flags for liblognorm like pcre2 did:
- gcc -c -Wall -Werror -fpic src/main/c/JavaLognorm.c -o src/main/c/JavaLognorm.o -I/usr/include/liblognorm -I/usr/include/json-c
- gcc -shared -Wl,-soname,JavaLognorm.so -o src/main/c/JavaLognorm.so src/main/c/JavaLognorm.o
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
- Autotools setup commands for this file:
- autoreconf --install
- ./configure
- make
+import static org.junit.jupiter.api.Assertions.*;
 
- None of the above matters if maven works as intended and executes the autotools scripts automatically.*/
+class JavaLognormTest {
 
-typedef struct OptionsStruct_TAG {
-    int CTXOPT_ALLOW_REGEX;
-    int CTXOPT_ADD_EXEC_PATH;
-    int CTXOPT_ADD_ORIGINALMSG;
-    int CTXOPT_ADD_RULE;
-    int CTXOPT_ADD_RULE_LOCATION;
-}OptionsStruct;
-
-const char *version() {
-    return ln_version();
-}
-
-void *initCtx() {
-    ln_ctx *ctx = malloc(sizeof(ln_ctx));
-    if((*ctx = ln_initCtx()) == NULL) {
-        // add exception handling here. ln_ returns null if error occurred.
-        return NULL;
+    @Test
+    public void versionTest() {
+        JavaLognorm javaLognorm = new JavaLognorm();
+        String s = javaLognorm.liblognormVersionCheck();
+        Assertions.assertEquals("2.0.6", s);
     }
-    return ctx;
-}
 
-void *inherittedCtx(ln_ctx parent) {
-    ln_ctx *ctx = malloc(sizeof(ln_ctx));
-    if((*ctx = ln_inherittedCtx(parent)) == NULL) {
-        // add exception handling here. ln_ returns null if error occurred.
-        return NULL;
-    }
-    return ctx;
 }
-
-void exitCtx(ln_ctx *context) {
-    if (*context) {
-        ln_exitCtx(*context);
-    }
-    free(context);
-}
-
-int loadSamples(ln_ctx *context, char *filename) {
-    return ln_loadSamples(*context, filename);
-}
-
-int loadSamplesFromString(ln_ctx *context, char *string) {
-    return ln_loadSamplesFromString(*context, string);
-}
-
